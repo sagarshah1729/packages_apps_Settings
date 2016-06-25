beb4e75c6a27762af8f72d49daa881d860a7d171
Adarsh-Mr <adarshmr1998@gmail.com>
2016-06-25 13:24:03 +0000

    Revert "Display: Volume Rocker Wake (2/2)"
    This reverts commit 37df4e792c2785cf93984eb4b35c8ea24aa73069.
    
    Conflicts:
    	res/values/custom_strings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -100,7 +100,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_CAMERA_GESTURE = "camera_gesture";
     private static final String KEY_CAMERA_DOUBLE_TAP_POWER_GESTURE
             = "camera_double_tap_power_gesture";
-    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -123,9 +122,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mAutoBrightnessPreference;
     private SwitchPreference mCameraGesturePreference;
     private SwitchPreference mCameraDoubleTapPowerGesturePreference;
-
-
-    private SwitchPreference mVolumeRockerWake;
 
     private ListPreference mLcdDensityPreference;
 
@@ -265,12 +261,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mNightModePreference.setValue(String.valueOf(currentNightMode));
             mNightModePreference.setOnPreferenceChangeListener(this);
         }
-
-        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
-        mVolumeRockerWake.setOnPreferenceChangeListener(this);
-        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
-                VOLUME_ROCKER_WAKE, 0);
-        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
     }
 
     private int getDefaultDensity() {
@@ -675,11 +665,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             } catch (NumberFormatException e) {
                 Log.e(TAG, "could not persist night mode setting", e);
             }
-        }
-        if (preference == mVolumeRockerWake) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
-                    value ? 1 : 0);
         }
         return true;
     }
