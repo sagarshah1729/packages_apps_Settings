a9621924f37bc49abb69b596757f72d28933fc1d
Martinusbe <martinusbe@gmail.com>
2016-06-27 07:45:30 +0000

    add global & securesettingsswitchpreference
    Change-Id: If998d4e1c8794cc815f6cedb75bdb195a5f24048
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- src/com/android/settings/cardinal/preference/SecureSettingSwitchPreference.java
+++ src/com/android/settings/cardinal/preference/SecureSettingSwitchPreference.java
@@ -1,6 +1,5 @@
-
 /*
- * Copyright (C) 2013 The CyanogenMod project
+ * Copyright (C) 2015 The CyanogenMod project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,20 +17,20 @@
 package com.android.settings.cardinal.preference;
 
 import android.content.Context;
-import android.preference.CheckBoxPreference;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
-public class SecureCheckBoxPreference extends CheckBoxPreference {
-    public SecureCheckBoxPreference(Context context, AttributeSet attrs, int defStyle) {
+public class SecureSettingSwitchPreference extends SwitchPreference {
+    public SecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
-    public SecureCheckBoxPreference(Context context, AttributeSet attrs) {
+    public SecureSettingSwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public SecureCheckBoxPreference(Context context) {
+    public SecureSettingSwitchPreference(Context context) {
         super(context, null);
     }
 
@@ -42,7 +41,6 @@ public class SecureCheckBoxPreference extends CheckBoxPreference {
                 // It's already there, so the same as persisting
                 return true;
             }
-
             Settings.Secure.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
             return true;
         }
@@ -54,7 +52,6 @@ public class SecureCheckBoxPreference extends CheckBoxPreference {
         if (!shouldPersist()) {
             return defaultReturnValue;
         }
-
         return Settings.Secure.getInt(getContext().getContentResolver(),
                 getKey(), defaultReturnValue ? 1 : 0) != 0;
     }
